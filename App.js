9744963b64d186dcd30e10f9167afd6b19f41079
Hong Tran <jessietran@fb.com>
2021-04-09 16:07:34 -0700

    got map and markers displayed

--- App.js
+++ App.js
@@ -1,12 +1,72 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+
+var markers = [
+  {
+    id: 1,
+    latitude : 37.6187 , 
+    longitude : 122.4924,
+    title: 'Foo Place',
+    subtitle: '1234 Foo Drive'
+  },
+  {
+    id: 2,
+    latitude : 37.6187 , 
+    longitude : 123.4924,
+    title: 'Foo Place',
+    subtitle: '1234 Foo Drive'
+  }
+];
+
+
 
 export default function App() {
+  const [markers, setMarkers] = useState([
+    {
+      id: 1,
+      latitude : 37.6187 , 
+      longitude : -122.4924,
+      title: 'Foo Place',
+      subtitle: '1234 Foo Drive'
+    },
+    {
+      id: 2,
+      latitude : 37.6287 , 
+      longitude : -122.5924,
+      title: 'Foo Place',
+      subtitle: '1234 Foo Drive'
+    }
+  ]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello world!</Text>
+      <Text>By the way Delivery</Text>
       <StatusBar style="auto" />
+      <MapView 
+        style={styles.map} 
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.051,
+        }}
+      >
+      {markers.map(marker =>
+        <MapView.Marker
+            coordinate={{
+                latitude: marker.latitude,
+                longitude: marker.longitude}}
+            title= {marker.title}
+            pinColor = {"red"}
+            description={marker.subtitle}
+         />
+      )}
+      </MapView>
     </View>
   );
 }
@@ -17,5 +77,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
